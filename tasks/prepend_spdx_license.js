7008d0eb1ad6086aef2fcce883a56e0fc90758de
Nick Barry <itsnickbarry@protonmail.ch>
2022-11-06 19:11:10 -0600

    parallelize prepend-spdx-license task

--- tasks/prepend_spdx_license.js
+++ tasks/prepend_spdx_license.js
@@ -6,29 +6,29 @@ const {
 } = require('hardhat/builtin-tasks/task-names');
 
 task('prepend-spdx-license', 'Prepends SPDX License identifier to local source files', async function (args, hre) {
-  let { license } = JSON.parse(fs.readFileSync(`${ hre.config.paths.root }/package.json`, 'utf8'));
+  const { license } = JSON.parse(fs.readFileSync(`${ hre.config.paths.root }/package.json`, 'utf8'));
 
   if (!license) {
     throw new HardhatPluginError('no license specified in package.json, unable to add SPDX License Identifier to sources');
   }
 
-  let sources = await hre.run(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS, args);
+  const sourcePaths = await hre.run(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS, args);
 
-  let headerBase = '// SPDX-License-Identifier:';
-  let regexp = new RegExp(`(${ headerBase }.*\n*)?`);
-  let header = `${ headerBase } ${ license }\n\n`;
+  const headerBase = '// SPDX-License-Identifier:';
+  const regexp = new RegExp(`(${ headerBase }.*\n*)?`);
+  const header = `${ headerBase } ${ license }\n\n`;
 
   let count = 0;
 
-  sources.forEach(function (absolutePath) {
+  await Promise.all(sourcePaths.map(async (sourcePath) => {
     // content is read from disk for preprocessor compatibility
-    let content = fs.readFileSync(absolutePath).toString();
+    const content = fs.readFileSync(sourcePath).toString();
 
     if (!content.startsWith(header) && (!content.startsWith(headerBase) || hre.config.spdxLicenseIdentifier.overwrite)) {
-      fs.writeFileSync(absolutePath, content.replace(regexp, header));
+      fs.writeFileSync(sourcePath, content.replace(regexp, header));
       count++;
     }
-  });
+  }));
 
   if (count > 0) {
     console.log(`Prepended SPDX License Identifier "${ license }" to ${ count } sources.`);
